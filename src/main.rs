0f159005ca8054ca671799a59ae163e06978c0be
Rahul Singh <rscodedocs@gmail.com>
2020-04-25 16:39:36 +0530

    comments in rust

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 // mod data_types;
 // mod strings;
-mod operators;
+// mod operators;
+mod comments;
 
 fn main() {
     println!("Hello from Rust 😊");
     // data_types::run();
     // strings::run();
-    operators::run();
+    // operators::run();
+    comments::run();
 }
